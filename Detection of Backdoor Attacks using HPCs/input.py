508a05b041d15075cb372fcb9bdbb5922a372bf8
GitAnkit-hub <ankitag26598@gmail.com>
2026-09-04 16:29:24 +0530

    Maths to find the relation in the given data

--- Detection of Backdoor Attacks using HPCs/input.py
+++ Detection of Backdoor Attacks using HPCs/input.py
@@ -2,11 +2,34 @@ import pandas as pd
 
 df = pd.read_csv("trace.csv")
 
-print(df.shape)
-print(df.columns)
+# print(df.shape)
+# print(df.columns)
 
-print(df.head())
+# print(df.head())
 
-print(df.describe())
+# print(df.describe())
 
-print(df.isnull().sum())
+# print(df.isnull().sum())
+
+
+# what does normla look alike ?
+# print(df.corr()) # correlation
+
+# #lets make scatter plots
+# import matplotlib.pyplot as plt
+
+# pd.plotting.scatter_matrix(df, figsize=(10,10))
+# plt.show()
+
+
+# lets look at covariance also
+
+
+print(df.cov())
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(df)
+
+print(X_scaled[:5])
